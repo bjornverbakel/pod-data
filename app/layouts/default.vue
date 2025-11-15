d2889fa1b267aecae98c9495a6e2170836e0ea35
Bjorn Verbakel <bjorn.verbakel@student.fontys.nl>
2025-11-15 23:01:02 +0100

    add more config and header

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,26 @@
 <template>
-  <v-main>
-    <v-container fluid class="fill-height d-flex align-center justify-center">
-      <slot />
-    </v-container>
-  </v-main>
+  <v-app>
+    <v-app-bar color="primary">
+      <template v-slot:prepend class="ga-4">
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      </template>
+
+      <v-app-bar-title class="text-h4">
+        NACC
+        <div v-if="$vuetify.display.smAndUp" class="text-caption">
+          NieR: Automata Completion Checklist
+        </div>
+      </v-app-bar-title>
+
+      <template v-slot:append>
+        <v-btn prepend-icon="mdi-login">Log in</v-btn>
+      </template>
+    </v-app-bar>
+
+    <v-main>
+      <v-container fluid class="fill-height d-flex align-center justify-center">
+        <slot></slot>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
--- vuetify.config.ts
+++ vuetify.config.ts
@@ -1,36 +1,46 @@
+import { VAppBarNavIcon } from "vuetify/components";
+
 export const vuetifyConfig = {
   // Define global component defaults
   defaults: {
     VBtn: {
       elevation: 0,
-      rounded: '0',
+      rounded: "0",
     },
     VCard: {
       elevation: 0,
-      rounded: '0',
+      rounded: "0",
+    },
+    VAppBar: {
+      elevation: 0,
+      rounded: "0",
+    },
+    VAppBarNavIcon: {
+      rounded: "pill",
     },
   },
 
   // Define themes
   theme: {
-    defaultTheme: 'light',
+    defaultTheme: "light",
     themes: {
       light: {
         colors: {
-          background: '#C4C0A9',
-          surface: '#D3CDB5',
-          primary: '#47453D',
-          secondary: '#C4C0A9',
-          accent: '#D3CDB5',
-          error: '#FF5252',
-          info: '#2196F3',
-          success: '#4CAF50',
-          warning: '#FB8C00',
-          text: '#47453D',
-          "on-surface": '#47453D',
-          "on-background": '#47453D'
-        }
-      }
-    }
-  }
-}
+          background: "#C4C0A9",
+          surface: "#D3CDB5",
+          primary: "#47453D",
+          secondary: "#C4C0A9",
+          accent: "#D3CDB5",
+          error: "#FF5252",
+          info: "#2196F3",
+          success: "#4CAF50",
+          warning: "#FB8C00",
+          text: "#47453D",
+          "on-surface": "#47453D",
+          "on-background": "#C4C0A9",
+          "on-primary": "#C4C0A9",
+        },
+      },
+    },
+  },
+};
