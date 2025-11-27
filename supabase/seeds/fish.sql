d5d1ba4928cef1edcc180674dbfd89051fc98e0d
Bjorn Verbakel <147508174+bjornverbakel@users.noreply.github.com>
2025-11-27 21:34:57 +0100

    Fix seed text[] formatting

--- supabase/seeds/fish.sql
+++ supabase/seeds/fish.sql
@@ -9,7 +9,7 @@ INSERT INTO "public"."fish" (
 VALUES (
         '025f52a8-4489-4265-a42a-0596dd8b61f0',
         'Arapaima',
-        '"{\"Forest Zone (Rivers/Waterfalls/Castle)\"]}',
+        '{"Forest Zone (Rivers/Waterfalls/Castle)"}',
         'https://nierautomata.wiki.fextralife.com/Arapaima',
         '2025-11-23 21:29:12.26808+00',
         '9'
@@ -17,7 +17,7 @@ VALUES (
     (
         '041a89c9-5fed-4c9d-b5e4-eaf18dc8eea6',
         'Arapaima Machine',
-        '"{\"City Ruins (Ravine)\",\"Amusement Park (Bridge)\",\"Forest (Rivers/Waterfalls/Castle)\"]}',
+        '{"City Ruins (Ravine)", "Amusement Park (Bridge)", "Forest (Rivers/Waterfalls/Castle)"}',
         'https://nierautomata.wiki.fextralife.com/Arapaima+Machine',
         '2025-11-23 21:29:12.26808+00',
         '24'
@@ -25,7 +25,7 @@ VALUES (
     (
         '0843baa9-598a-4c96-9f0f-f5cc3004729b',
         'Blowfish',
-        '"{\"Flooded City (Coast/Missile Area)\"]}',
+        '{"Flooded City (Coast/Missile Area)"}',
         'https://nierautomata.wiki.fextralife.com/Blowfish',
         '2025-11-23 21:29:12.26808+00',
         '15'
@@ -33,7 +33,7 @@ VALUES (
     (
         '23ff607a-b201-4bc4-b531-90e6dd96850a',
         'Swordfish Machine',
-        '"{\"Flooded City (Missile Area)\"]}',
+        '{"Flooded City (Missile Area)"}',
         'https://nierautomata.wiki.fextralife.com/Swordfish+Machine',
         '2025-11-23 21:29:12.26808+00',
         '28'
@@ -41,7 +41,7 @@ VALUES (
     (
         '24434582-835e-4ccd-9304-3335761ec18a',
         'Blowfish Machine',
-        '"{\"Desert Zone (Oil Field)\",\"Flooded City (Coast/Missile Area)\"]}',
+        '{"Desert Zone (Oil Field)", "Flooded City (Coast/Missile Area)"}',
         'https://nierautomata.wiki.fextralife.com/Blowfish+Machine',
         '2025-11-23 21:29:12.26808+00',
         '27'
@@ -49,7 +49,7 @@ VALUES (
     (
         '27477d4b-f7b2-4d7a-8798-51a4ac7a0f8d',
         'Twoface',
-        '"{\"Flooded City (Coast/Missile Area)\"]}',
+        '{"Flooded City (Coast/Missile Area)"}',
         'https://nierautomata.wiki.fextralife.com/Twoface',
         '2025-11-23 21:29:12.26808+00',
         '13'
@@ -57,7 +57,7 @@ VALUES (
     (
         '275cd7aa-da98-4812-9585-01f278e1532c',
         'Killifish',
-        '"{\"City Ruins (Streams/Waterfalls/Lake)\",\"Desert (Oil Field/Oasis)\",\"Amusement Park (Bridge)\",\"Forest (Rivers/Waterfalls/Castle)\",\"Flooded City (Waterfall Pond)\"]}',
+        '{"City Ruins (Streams/Waterfalls/Lake)", "Desert (Oil Field/Oasis)", "Amusement Park (Bridge)", "Forest (Rivers/Waterfalls/Castle)", "Flooded City (Waterfall Pond)"}',
         'https://nierautomata.wiki.fextralife.com/Killifish',
         '2025-11-23 21:29:12.26808+00',
         '3'
@@ -65,7 +65,7 @@ VALUES (
     (
         '29ba69fa-7061-47b3-ae6d-495046c09556',
         'Beetle Fish',
-        '"{\"Desert Zone (Oasis)\"]}',
+        '{"Desert Zone (Oasis)"}',
         'https://nierautomata.wiki.fextralife.com/Beetle+Fish',
         '2025-11-23 21:29:12.26808+00',
         '11'
@@ -73,7 +73,7 @@ VALUES (
     (
         '32c9ad6c-a86b-4218-88e7-8a05d67a6283',
         'Bream Machine',
-        '"{\"Desert Zone (Oil Field)\",\"Flooded City (Coast/Missile Area)\"]}',
+        '{"Desert Zone (Oil Field)", "Flooded City (Coast/Missile Area)"}',
         'https://nierautomata.wiki.fextralife.com/Bream+Machine',
         '2025-11-23 21:29:12.26808+00',
         '30'
@@ -81,7 +81,7 @@ VALUES (
     (
         '4ba90ac6-8cc5-46a4-83ff-61839194da28',
         'Killifish Machine',
-        '"{\"City Ruins (Streams/Waterfalls)\",\"Forest (Rivers/Waterfalls/Castle)\",\"Flooded City (Waterfall Pond)\"]}',
+        '{"City Ruins (Streams/Waterfalls)", "Forest (Rivers/Waterfalls/Castle)", "Flooded City (Waterfall Pond)"}',
         'https://nierautomata.wiki.fextralife.com/Killifish+Machine',
         '2025-11-23 21:29:12.26808+00',
         '22'
@@ -89,7 +89,7 @@ VALUES (
     (
         '53d3f6a3-c023-4d40-b1bb-7bdf1ea44cbe',
         'Tire',
-        '"{\"Sewers (Amusement Park Route)\",\"Flooded City (Coast/Missile Area)\"]}',
+        '{"Sewers (Amusement Park Route)", "Flooded City (Coast/Missile Area)"}',
         'https://nierautomata.wiki.fextralife.com/Tire',
         '2025-11-23 21:29:12.26808+00',
         '38'
@@ -97,7 +97,7 @@ VALUES (
     (
         '5b7c02fa-ead8-47ea-b210-1bc4d8a17e95',
         'Bream',
-        '"{\"Flooded City (Coast/Missile Area)\"]}',
+        '{"Flooded City (Coast/Missile Area)"}',
         'https://nierautomata.wiki.fextralife.com/Bream',
         '2025-11-23 21:29:12.26808+00',
         '20'
@@ -105,7 +105,7 @@ VALUES (
     (
         '5e58cf47-b70a-42ec-8d81-2445f1893332',
         'Arowana',
-        '"{\"City Ruins (Underground Lake)\",\"Desert Zone (Oasis)\",\"Forest Zone (Rivers/Waterfalls)\"]}',
+        '{"City Ruins (Underground Lake)", "Desert Zone (Oasis)", "Forest Zone (Rivers/Waterfalls)"}',
         'https://nierautomata.wiki.fextralife.com/Arowana',
         '2025-11-23 21:29:12.26808+00',
         '1'
@@ -113,7 +113,7 @@ VALUES (
     (
         '645ebf77-8b6e-44cd-8c7b-34634aee621a',
         'Machine Lifeform Head',
-        '"{\"City Ruins (Streams/Waterfalls/Lake)\",\"Desert (Oil Field)\",\"Amusement Park (Bridge)\",\"Forest (Castle Front)\",\"Flooded City (Coast/Missile Area)\"]}',
+        '{"City Ruins (Streams/Waterfalls/Lake)", "Desert (Oil Field)", "Amusement Park (Bridge)", "Forest (Castle Front)", "Flooded City (Coast/Missile Area)"}',
         'https://nierautomata.wiki.fextralife.com/Machine+Lifeform+Head',
         '2025-11-23 21:29:12.26808+00',
         '37'
@@ -121,7 +121,7 @@ VALUES (
     (
         '67026810-d318-4c1e-bdca-2a5a32e14e96',
         'Starfish',
-        '"{\"Flooded City (Coast/Submerged Buildings)\"]}',
+        '{"Flooded City (Coast/Submerged Buildings)"}',
         'https://nierautomata.wiki.fextralife.com/Starfish',
         '2025-11-23 21:29:12.26808+00',
         '19'
@@ -129,7 +129,7 @@ VALUES (
     (
         '69e80987-7909-4521-95fb-d62fdf2a5374',
         'Carp',
-        '"{\"City Ruins (Streams/Waterfalls/Lake)\",\"Desert (Oasis)\",\"Amusement Park (Bridge)\",\"Forest (Rivers/Waterfalls/Castle)\",\"Flooded City (Waterfall Pond)\"]}',
+        '{"City Ruins (Streams/Waterfalls/Lake)", "Desert (Oasis)", "Amusement Park (Bridge)", "Forest (Rivers/Waterfalls/Castle)", "Flooded City (Waterfall Pond)"}',
         'https://nierautomata.wiki.fextralife.com/Carp',
         '2025-11-23 21:29:12.26808+00',
         '4'
@@ -137,7 +137,7 @@ VALUES (
     (
         '6cd132d1-7444-48ca-85e9-c1bfd768575c',
         'Coelacanth',
-        '"{\"Flooded City (Coast/Missile Area)\"]}',
+        '{"Flooded City (Coast/Missile Area)"}',
         'https://nierautomata.wiki.fextralife.com/Coelacanth',
         '2025-11-23 21:29:12.26808+00',
         '14'
@@ -145,7 +145,7 @@ VALUES (
     (
         '6e68d9f6-9a5e-47cc-a29f-0a122e7b2268',
         'Starfish Machine',
-        '"{\"Desert Zone (Oil Field)\",\"Flooded City (Coast)\"]}',
+        '{"Desert Zone (Oil Field)", "Flooded City (Coast)"}',
         'https://nierautomata.wiki.fextralife.com/Starfish+Machine',
         '2025-11-23 21:29:12.26808+00',
         '29'
@@ -153,7 +153,7 @@ VALUES (
     (
         '72e85f97-48b9-48aa-9adc-5c93dee52738',
         'Battery',
-        '"{\"Sewers (Amusement Park Route)\",\"Desert Zone (Oil Field)\"]}',
+        '{"Sewers (Amusement Park Route)", "Desert Zone (Oil Field)"}',
         'https://nierautomata.wiki.fextralife.com/Battery',
         '2025-11-23 21:29:12.26808+00',
         '40'
@@ -161,7 +161,7 @@ VALUES (
     (
         '7879e415-96cf-4097-a4b9-e91322d9d29e',
         'Broken Firearm',
-        '"{\"Sewers (Amusement Park Route)\",\"Amusement Park (Bridge)\"]}',
+        '{"Sewers (Amusement Park Route)", "Amusement Park (Bridge)"}',
         'https://nierautomata.wiki.fextralife.com/Broken+Firearm',
         '2025-11-23 21:29:12.26808+00',
         '41'
@@ -169,7 +169,7 @@ VALUES (
     (
         '7ab915e2-03f8-4ef1-add6-3b9d2366bfff',
         'Bloat Fish',
-        '"{\"City Ruins (Underground Lake)\"]}',
+        '{"City Ruins (Underground Lake)"}',
         'https://nierautomata.wiki.fextralife.com/Bloat+Fish',
         '2025-11-23 21:29:12.26808+00',
         '5'
@@ -177,7 +177,7 @@ VALUES (
     (
         '80fb9a13-ad90-4d4e-a2d7-e311ff9bc8fe',
         'Water Flea',
-        '"{\"Flooded City (Coast/Missile Area)\"]}',
+        '{"Flooded City (Coast/Missile Area)"}',
         'https://nierautomata.wiki.fextralife.com/Water+Flea',
         '2025-11-23 21:29:12.26808+00',
         '12'
@@ -185,7 +185,7 @@ VALUES (
     (
         '830b5b2f-5580-4784-bb1b-1d9ec5faa7c6',
         'Koi Carp',
-        '"{\"City Ruins (Streams/Waterfalls/Lake)\",\"Desert (Oasis)\",\"Amusement Park (Bridge)\",\"Forest (Rivers/Waterfalls/Castle)\",\"Flooded City (Waterfall Pond)\"]}',
+        '{"City Ruins (Streams/Waterfalls/Lake)", "Desert (Oasis)", "Amusement Park (Bridge)", "Forest (Rivers/Waterfalls/Castle)", "Flooded City (Waterfall Pond)"}',
         'https://nierautomata.wiki.fextralife.com/Koi+Carp',
         '2025-11-23 21:29:12.26808+00',
         '6'
@@ -193,7 +193,7 @@ VALUES (
     (
         '83639626-1053-4a0e-b619-9d20d9548b53',
         'Freshwater Ray Machine',
-        '"{\"Amusement Park (Bridge)\"]}',
+        '{"Amusement Park (Bridge)"}',
         'https://nierautomata.wiki.fextralife.com/Freshwater+Ray+Machine',
         '2025-11-23 21:29:12.26808+00',
         '36'
@@ -201,7 +201,7 @@ VALUES (
     (
         '8c067f07-68a0-489d-9928-3aef8242b95c',
         'Swordfish',
-        '"{\"Flooded City (Missile Area)\"]}',
+        '{"Flooded City (Missile Area)"}',
         'https://nierautomata.wiki.fextralife.com/Swordfish',
         '2025-11-23 21:29:12.26808+00',
         '16'
@@ -209,7 +209,7 @@ VALUES (
     (
         '8f45770a-4d8b-4990-8a7c-ac95b07ce8ef',
         'Oil Sardine',
-        '"{\"Desert Zone (Oil Field)\"]}',
+        '{"Desert Zone (Oil Field)"}',
         'https://nierautomata.wiki.fextralife.com/Oil+Sardine',
         '2025-11-23 21:29:12.26808+00',
         '10'
@@ -217,7 +217,7 @@ VALUES (
     (
         '9df72504-01c1-4bef-99dd-7f5e0f526348',
         'Horseshoe Crab Machine',
-        '"{\"Flooded City (Coast)\"]}',
+        '{"Flooded City (Coast)"}',
         'https://nierautomata.wiki.fextralife.com/Horseshoe+Crab+Machine',
         '2025-11-23 21:29:12.26808+00',
         '33'
@@ -225,7 +225,7 @@ VALUES (
     (
         'acf61ba1-d9d0-4062-adfe-b8ecc5cd8ed1',
         'Mackerel Machine',
-        '"{\"Desert Zone (Oil Field)\",\"Flooded City (Coast/Missile Area)\"]}',
+        '{"Desert Zone (Oil Field)", "Flooded City (Coast/Missile Area)"}',
         'https://nierautomata.wiki.fextralife.com/Mackerel+Machine',
         '2025-11-23 21:29:12.26808+00',
         '32'
@@ -233,7 +233,7 @@ VALUES (
     (
         'af6fd875-729a-44c4-bfb2-eb340c78f770',
         'Twinfish',
-        '"{\"City Ruins (Underground Lake)\"]}',
+        '{"City Ruins (Underground Lake)"}',
         'https://nierautomata.wiki.fextralife.com/Twinfish',
         '2025-11-23 21:29:12.26808+00',
         '2'
@@ -241,7 +241,7 @@ VALUES (
     (
         'b56e7edd-a527-4fb3-aeda-d99e6451a4db',
         'Bloat Fish Machine',
-        '"{\"City Ruins (Ravine)\",\"Desert (Oil Field)\",\"Forest (Rivers/Waterfalls/Castle)\"]}',
+        '{"City Ruins (Ravine)", "Desert (Oil Field)", "Forest (Rivers/Waterfalls/Castle)"}',
         'https://nierautomata.wiki.fextralife.com/Bloat+Fish+Machine',
         '2025-11-23 21:29:12.26808+00',
         '26'
@@ -249,7 +249,7 @@ VALUES (
     (
         'bba8775a-99b7-4fb1-8209-62a5b65703e8',
         'Gas Cylinder',
-        '"{\"City Ruins (Ravine)\",\"Sewers (Amusement Park Route)\",\"Forest (Rivers/Waterfalls/Castle)\"]}',
+        '{"City Ruins (Ravine)", "Sewers (Amusement Park Route)", "Forest (Rivers/Waterfalls/Castle)"}',
         'https://nierautomata.wiki.fextralife.com/Gas+Cylinder',
         '2025-11-23 21:29:12.26808+00',
         '39'
@@ -257,7 +257,7 @@ VALUES (
     (
         'bbc7668c-454c-4c36-bed9-11c6192d5de7',
         'Coelacanth Machine',
-        '"{\"Flooded City (Missile Area)\"]}',
+        '{"Flooded City (Missile Area)"}',
         'https://nierautomata.wiki.fextralife.com/Coelacanth+Machine',
         '2025-11-23 21:29:12.26808+00',
         '31'
@@ -265,7 +265,7 @@ VALUES (
     (
         'bff1d079-e187-49c9-959f-8e70a34e86e9',
         'Arowana Machine',
-        '"{\"City Ruins (Ravine)\"]}',
+        '{"City Ruins (Ravine)"}',
         'https://nierautomata.wiki.fextralife.com/Arowana+Machine',
         '2025-11-23 21:29:12.26808+00',
         '34'
@@ -273,7 +273,7 @@ VALUES (
     (
         'c23e13e5-b8ba-41df-8833-7b2d33e69390',
         'Horseshoe Crab',
-        '"{\"Flooded City (Coast)\"]}',
+        '{"Flooded City (Coast)"}',
         'https://nierautomata.wiki.fextralife.com/Horseshoe+Crab',
         '2025-11-23 21:29:12.26808+00',
         '18'
@@ -281,7 +281,7 @@ VALUES (
     (
         'c2470118-0a71-48ef-8964-b84373e2a586',
         'Mackerel',
-        '"{\"Received from NPC Jackass\",\"Flooded City (Coast/Missile Area)\"]}',
+        '{"Received from NPC Jackass", "Flooded City (Coast/Missile Area)"}',
         'https://nierautomata.wiki.fextralife.com/Mackerel',
         '2025-11-23 21:29:12.26808+00',
         '17'
@@ -289,7 +289,7 @@ VALUES (
     (
         'dac42d8c-ac58-4c38-b21f-1a8c11601722',
         'Basking Shark Machine',
-        '"{\"Flooded City (Missile Area)\"]}',
+        '{"Flooded City (Missile Area)"}',
         'https://nierautomata.wiki.fextralife.com/Basking+Shark+Machine',
         '2025-11-23 21:29:12.26808+00',
         '35'
@@ -297,7 +297,7 @@ VALUES (
     (
         'ddd1452a-c543-49bd-9167-821c80176c6e',
         'Basking Shark',
-        '"{\"Flooded City (Missile Area)\"]}',
+        '{"Flooded City (Missile Area)"}',
         'https://nierautomata.wiki.fextralife.com/Basking+Shark',
         '2025-11-23 21:29:12.26808+00',
         '21'
@@ -305,7 +305,7 @@ VALUES (
     (
         'f04ed2ea-e8b4-4c9c-8be5-e1274f536273',
         'Koi Carp Machine',
-        '"{\"City Ruins (Streams/Waterfalls)\",\"Desert (Oil Field)\",\"Amusement Park (Bridge)\",\"Forest (Rivers/Waterfalls/Castle)\",\"Flooded City (Waterfall Pond)\"]}',
+        '{"City Ruins (Streams/Waterfalls)", "Desert (Oil Field)", "Amusement Park (Bridge)", "Forest (Rivers/Waterfalls/Castle)", "Flooded City (Waterfall Pond)"}',
         'https://nierautomata.wiki.fextralife.com/Koi+Carp+Machine',
         '2025-11-23 21:29:12.26808+00',
         '23'
@@ -313,7 +313,7 @@ VALUES (
     (
         'f36b451c-3856-4281-9db5-e7331cea183e',
         'Fur Carp',
-        '"{\"City Ruins (Ravine)\",\"Forest Zone (Rivers/Waterfalls/Castle)\"]}',
+        '{"City Ruins (Ravine)", "Forest Zone (Rivers/Waterfalls/Castle)"}',
         'https://nierautomata.wiki.fextralife.com/Fur+Carp',
         '2025-11-23 21:29:12.26808+00',
         '7'
@@ -321,7 +321,7 @@ VALUES (
     (
         'f454ff08-1696-4862-982c-653db612a914',
         'Freshwater Ray',
-        '"{\"Amusement Park (Bridge)\"]}',
+        '{"Amusement Park (Bridge)"}',
         'https://nierautomata.wiki.fextralife.com/Freshwater+Ray',
         '2025-11-23 21:29:12.26808+00',
         '8'
@@ -329,7 +329,7 @@ VALUES (
     (
         'f491dbd4-29dc-4593-b62c-37f7053ef377',
         'Carp Machine',
-        '"{\"City Ruins (Streams/Waterfalls)\",\"Desert (Oil Field)\",\"Forest (Rivers/Waterfalls/Castle)\",\"Flooded City (Waterfall Pond)\"]}',
+        '{"City Ruins (Streams/Waterfalls)", "Desert (Oil Field)", "Forest (Rivers/Waterfalls/Castle)", "Flooded City (Waterfall Pond)"}',
         'https://nierautomata.wiki.fextralife.com/Carp+Machine',
         '2025-11-23 21:29:12.26808+00',
         '25'
